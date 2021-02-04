f7d1f669905dfaebd5b4124351b244ba2deb0908
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-05 00:37:44 +0600

    Create recursive factorial function

--- F#/Program.fs
+++ F#/Program.fs
@@ -2,11 +2,12 @@
 
 open System
 
-let do_functs =
+let do_functs() =
     let sum(a: int, b: int): int = a + b
     printf "the sum of 5 and 12 is %i" (sum(5, 12))
 
-        
+let rec factorial n = if n <= 1 then 1 else n * factorial(n - 1)
+
 let introduce() =
     printf "What is your name?"
     let name = Console.ReadLine()
@@ -17,6 +18,7 @@ let introduce() =
 [<EntryPoint>]
 let main argv =
     // introduce()
-    do_functs
+    // do_functs
+    printf "factorial of 5 is %i " (factorial 5)
     Console.ReadKey() |> ignore
     0 // return an integer exit code
