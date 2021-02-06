cc66d97fd421835a4e02b30b0a1c45408917a864
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2021-02-06 17:42:44 +0600

    struct stuff

--- F#/Program.fs
+++ F#/Program.fs
@@ -207,6 +207,19 @@ let generic_stuff() =
     // printfn "sumo of 2.3 and 1.2 is %.2f" (add_stuff<float> 2.3 1.2)
     printfn "sumo of 2 and 1 is %i" (add_stuff<int> 2 1)
 
+// let exception_handling() = 
+type Rectangle = struct 
+    val length : int
+    val width : int
+
+    new (l, w) =
+        { length = l; width = w}
+end
+let struc_stuff() = 
+    let area (r: Rectangle) = 
+        r.length * r.width
+    let a = new Rectangle(4, 5)
+    printfn "area is %i" (area a)
 [<EntryPoint>]
 let main argv =
     // introduce()
@@ -235,6 +248,8 @@ let main argv =
     // record_stuff()
     // seq_stuff()
     // map_stuff()
-    generic_stuff()
+    // generic_stuff()
+    // exception_handling()
+    struc_stuff()
     Console.ReadKey() |> ignore
     0 // return an integer exit code
